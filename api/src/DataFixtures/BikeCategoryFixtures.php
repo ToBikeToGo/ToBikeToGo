e452eabb97e2513f6d7a8566751cb781003e5295
Vivian Ruhlmann <vivian.fr@free.fr>
2024-02-08 22:19:51 +0100

    ♻️(Refactor): Clean code for SonarQube

--- api/src/DataFixtures/BikeCategoryFixtures.php
+++ api/src/DataFixtures/BikeCategoryFixtures.php
@@ -12,198 +12,41 @@ class BikeCategoryFixtures extends Fixture
 
     public function load(ObjectManager $manager): void
     {
-        $object = (new BikeCategory())
-            ->setName('Ville')
-            ->setType('Hollandais');
-
-        $manager->persist($object);
-
-        $this->addReference(self::BIKE_CATEGORY_REFERENCE . 0, $object);
-
-        $object = (new BikeCategory())
-            ->setName('Ville')
-            ->setType('Vintage');
-
-        $manager->persist($object);
-
-        $this->addReference(self::BIKE_CATEGORY_REFERENCE . 1, $object);
-
-        $object = (new BikeCategory())
-            ->setName('Ville')
-            ->setType('VTC');
-
-        $manager->persist($object);
-
-        $this->addReference(self::BIKE_CATEGORY_REFERENCE . 2, $object);
-
-        $object = (new BikeCategory())
-            ->setName('Ville')
-            ->setType('Pliant');
-
-        $manager->persist($object);
-
-        $this->addReference(self::BIKE_CATEGORY_REFERENCE . 3, $object);
-
-        $object = (new BikeCategory())
-            ->setName('Ville')
-            ->setType('Fixie');
-
-        $manager->persist($object);
-
-        $this->addReference(self::BIKE_CATEGORY_REFERENCE . 4, $object);
-
-        $object = (new BikeCategory())
-            ->setName('Ville')
-            ->setType('Tandem');
-
-        $manager->persist($object);
-
-        $this->addReference(self::BIKE_CATEGORY_REFERENCE . 5, $object);
-
-        $object = (new BikeCategory())
-            ->setName('Ville')
-            ->setType('Cargo');
-
-        $manager->persist($object);
-
-        $this->addReference(self::BIKE_CATEGORY_REFERENCE . 6, $object);
-
-        $object = (new BikeCategory())
-            ->setName('Route')
-            ->setType('VTT');
-
-        $manager->persist($object);
-
-        $this->addReference(self::BIKE_CATEGORY_REFERENCE . 7, $object);
-
-        $object = (new BikeCategory())
-            ->setName('Route')
-            ->setType('Route');
-
-        $manager->persist($object);
-
-        $this->addReference(self::BIKE_CATEGORY_REFERENCE . 8, $object);
-
-        $object = (new BikeCategory())
-            ->setName('Route')
-            ->setType('Gravel');
-
-        $manager->persist($object);
-
-        $this->addReference(self::BIKE_CATEGORY_REFERENCE . 9, $object);
-
-        $object = (new BikeCategory())
-            ->setName('Route')
-            ->setType('Cyclo-cross');
-
-        $manager->persist($object);
-
-        $this->addReference(self::BIKE_CATEGORY_REFERENCE . 10, $object);
-
-        $object = (new BikeCategory())
-            ->setName('Enfant')
-            ->setType('Ville');
-
-        $manager->persist($object);
-
-        $this->addReference(self::BIKE_CATEGORY_REFERENCE . 11, $object);
-
-        $object = (new BikeCategory())
-            ->setName('Couché')
-            ->setType('Deux roues');
-
-        $manager->persist($object);
-
-        $this->addReference(self::BIKE_CATEGORY_REFERENCE . 12, $object);
-
-        $object = (new BikeCategory())
-            ->setName('Couché')
-            ->setType('Trois roues');
-
-        $manager->persist($object);
-
-        $this->addReference(self::BIKE_CATEGORY_REFERENCE . 13, $object);
-
-        // BMX
-        $object = (new BikeCategory())
-            ->setName('BMX')
-            ->setType('Race');
-
-        $manager->persist($object);
-
-        $this->addReference(self::BIKE_CATEGORY_REFERENCE . 14, $object);
-
-        $object = (new BikeCategory())
-            ->setName('BMX')
-            ->setType('Flat');
-
-        $manager->persist($object);
-
-        $this->addReference(self::BIKE_CATEGORY_REFERENCE . 15, $object);
-
-        $object = (new BikeCategory())
-            ->setName('BMX')
-            ->setType('Dirt');
-
-        $manager->persist($object);
-
-        $this->addReference(self::BIKE_CATEGORY_REFERENCE . 16, $object);
-
-        $object = (new BikeCategory())
-            ->setName('BMX')
-            ->setType('Street');
-
-        $manager->persist($object);
-
-        $this->addReference(self::BIKE_CATEGORY_REFERENCE . 17, $object);
-
-        $object = (new BikeCategory())
-            ->setName('BMX')
-            ->setType('Park');
-
-        $manager->persist($object);
-
-        $this->addReference(self::BIKE_CATEGORY_REFERENCE . 18, $object);
-
-        $object = (new BikeCategory())
-            ->setName('BMX')
-            ->setType('Trail');
-
-        $manager->persist($object);
-
-        $this->addReference(self::BIKE_CATEGORY_REFERENCE . 19, $object);
-
-        $object = (new BikeCategory())
-            ->setName('Route')
-            ->setType('Endurance');
-
-        $manager->persist($object);
-
-        $this->addReference(self::BIKE_CATEGORY_REFERENCE . 20, $object);
-
-        $object = (new BikeCategory())
-            ->setName('Route')
-            ->setType('Aéro');
-
-        $manager->persist($object);
-
-        $this->addReference(self::BIKE_CATEGORY_REFERENCE . 21, $object);
-
-        $object = (new BikeCategory())
-            ->setName('Route')
-            ->setType('Voyage');
-
-        $manager->persist($object);
-
-        $this->addReference(self::BIKE_CATEGORY_REFERENCE . 22, $object);
-
-        $object = (new BikeCategory())
-            ->setName('Route')
-            ->setType('Montagne');
-
-        $manager->persist($object);
-
-        $this->addReference(self::BIKE_CATEGORY_REFERENCE . 23, $object);
+        $categories = [
+            ['City', 'Dutch'],
+            ['City', 'Vintage'],
+            ['City', 'Hybrid'],
+            ['City', 'Folding'],
+            ['City', 'Fixed Gear'],
+            ['City', 'Tandem'],
+            ['City', 'Cargo'],
+            ['Road', 'MTB'],
+            ['Road', 'Road'],
+            ['Road', 'Gravel'],
+            ['Road', 'Cyclocross'],
+            ['Road', 'Endurance'],
+            ['Road', 'Aero'],
+            ['Road', 'Touring'],
+            ['Road', 'Mountain'],
+            ['Kids', 'City'],
+            ['Recumbent', 'Two-wheeler'],
+            ['Recumbent', 'Three-wheeler'],
+            ['BMX', 'Race'],
+            ['BMX', 'Flat'],
+            ['BMX', 'Dirt'],
+            ['BMX', 'Street'],
+            ['BMX', 'Park'],
+            ['BMX', 'Trail']
+        ];
+
+        foreach ($categories as $index => $category) {
+            $object = (new BikeCategory())
+                ->setName($category[0])
+                ->setType($category[1]);
+
+            $manager->persist($object);
+            $this->addReference(self::BIKE_CATEGORY_REFERENCE . $index, $object);
+        }
 
         $manager->flush();
     }
