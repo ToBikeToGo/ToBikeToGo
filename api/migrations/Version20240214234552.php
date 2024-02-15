0c4289bc278e4d76f8d56e406f35a9abf6b0f69f
LailaCharaoui <mats2@live.fr>
2024-02-16 00:36:42 +0100

    feat :sparkles: (front): add migrations

---
+++ api/migrations/Version20240214234552.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240214234552 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE shop ADD longitude DOUBLE PRECISION DEFAULT NULL');
+        $this->addSql('ALTER TABLE shop ADD latitude DOUBLE PRECISION DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('CREATE SCHEMA publci');
+        $this->addSql('ALTER TABLE shop DROP longitude');
+        $this->addSql('ALTER TABLE shop DROP latitude');
+    }
+}
