7981e0e39684548e306726a50fcb71c5e9f7eb49
Vivian Ruhlmann <vivian.fr@free.fr>
2024-02-16 08:24:38 +0100

    🔒️(Security): Verify owner

---
+++ api/src/EventSubscriber/IsOwnerSubscriber.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class IsOwnerSubscriber implements EventSubscriberInterface
+{
+
+    public function __construct(private TokenStorageInterface $tokenStorage)
+    {
+    }
+
+    public function onKernelRequest(RequestEvent $event): void
+    {
+        $loggedInUser = $this->tokenStorage->getToken()->getUser();
+        $roles = $loggedInUser->getRoles();
+
+        if (in_array('ROLE_ADMIN', $roles) || in_array('ROLE_PROVIDER', $roles)) {
+            return;
+        }
+
+        $requestUri = $event->getRequest()->getRequestUri();
+
+        if (str_contains($requestUri, '/api/users/')) {
+            $requestUser = $event->getRequest()->get('data');
+            if ($loggedInUser !== $requestUser) {
+                throw new CustomUserMessageAuthenticationException('You are not the owner of this resource');
+            }
+        }
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::REQUEST => 'onKernelRequest',
+        ];
+    }
+}
